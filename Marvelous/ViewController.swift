e6efbc1605b3992d67dbe2cd110ca6d56042ae02
Victor Hideo Oka <oka.victor97@gmail.com>
2021-08-25 18:45:55 -0300

    initial commit

---
+++ Marvelous/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Marvelous
+//
+//  Created by Victor Oka on 25/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    override func loadView() {
+        let view = UIView(frame: UIScreen.main.bounds)
+        view.backgroundColor = .red
+        self.view = view
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-Marvelous-MarvelousUITests/Pods-Marvelous-MarvelousUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Marvelous_MarvelousUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Marvelous_MarvelousUITests
+@end
